ad51579ef39e04756b47beae473dd79cb498cd2d
HoAngAnh24-PNH <phamnhuhoang24102004@gmail.com>
2024-11-18 00:26:06 +0700

    AddData_TaiTro.sql

--- BTL-CSDL/SQL/AddData_TaiTro.sql
+++ BTL-CSDL/SQL/AddData_TaiTro.sql
@@ -1,61 +1,61 @@
-﻿-- Thêm dữ liệu vào bảng TaiTro
+-- Thêm dữ liệu vào bảng TaiTro
 INSERT INTO TaiTro (TenGiai, MuaGiai, MaNhaTaiTro, BatDau, KetThuc, HinhThuc) VALUES
 ('NBA Finals', '2023-2024', 'NTT01', '2021-02-03', '2025-08-19', 'Tien'),
-('West Conf Finals', '2023-2024', 'NTT01', '2022-10-24', '2026-12-09', 'Tien'),
+('Western Conference Finals', '2023-2024', 'NTT01', '2022-10-24', '2026-12-09', 'Tien'),
 
 ('NBA Finals', '2023-2024', 'NTT02', '2022-10-07', '2025-06-21', 'Tien'),
-('East Conf Finals', '2023-2024', 'NTT02', '2022-10-01', '2024-05-20', 'Tien'),
+('Eastern Conference Finals', '2023-2024', 'NTT02', '2022-10-01', '2024-05-20', 'Tien'),
 
 ('NBA Finals', '2023-2024', 'NTT03', '2022-12-15', '2025-04-11', 'Tien'),
-('West Conf Finals', '2023-2024', 'NTT03', '2021-10-01', '2024-03-12', 'San pham'),
+('Western Conference Finals', '2023-2024', 'NTT03', '2021-10-01', '2024-03-12', 'San pham'),
 
-('East Conf Finals', '2023-2024', 'NTT04', '2022-10-01', '2025-11-14', 'Tien'),
+('Eastern Conference Finals', '2023-2024', 'NTT04', '2022-10-01', '2025-11-14', 'Tien'),
 ('NBA Finals', '2023-2024', 'NTT04', '2023-10-18', '2024-04-29', 'San pham'),
 
-('West Conf Finals', '2023-2024', 'NTT05', '2022-10-06', '2025-08-20', 'Tien'),
-('East Conf Finals', '2023-2024', 'NTT05', '2023-05-01', '2027-05-20', 'San pham'),
+('Western Conference Finals', '2023-2024', 'NTT05', '2022-10-06', '2025-08-20', 'Tien'),
+('Eastern Conference Finals', '2023-2024', 'NTT05', '2023-05-01', '2027-05-20', 'San pham'),
 
 ('NBA Finals', '2023-2024', 'NTT06', '2021-10-12', '2024-04-13', 'Hinh anh'),
-('West Conf Finals', '2023-2024', 'NTT06', '2022-09-01', '2025-05-28', 'San pham'),
+('Western Conference Finals', '2023-2024', 'NTT06', '2022-09-01', '2025-05-28', 'San pham'),
 
-('East Conf Finals', '2023-2024', 'NTT07', '2024-01-08', '2026-07-16', 'Truyen thong'),
+('Eastern Conference Finals', '2023-2024', 'NTT07', '2024-01-08', '2026-07-16', 'Truyen thong'),
 ('NBA Finals', '2023-2024', 'NTT07', '2021-10-01', '2024-06-09', 'San pham'),
 
-('West Conf Finals', '2023-2024', 'NTT08', '2022-12-01', '2024-07-30', 'Truyen thong'),
-('East Conf Finals', '2023-2024', 'NTT08', '2022-03-11','2024-09-25', 'Tien'),
+('Western Conference Finals', '2023-2024', 'NTT08', '2022-12-01', '2024-07-30', 'Truyen thong'),
+('Eastern Conference Finals', '2023-2024', 'NTT08', '2022-03-11','2024-09-25', 'Tien'),
 
 ('NBA Finals', '2023-2024', 'NTT09', '2021-08-19', '2024-01-12', 'Tien'),
-('West Conf Finals', '2023-2024', 'NTT09', '2023-10-18', '2025-07-20', 'Tien'),
+('Western Conference Finals', '2023-2024', 'NTT09', '2023-10-18', '2025-07-20', 'Tien'),
 
-('East Conf Finals', '2023-2024', 'NTT10', '2022-10-24', '2026-11-23', 'Truyen thong'),
+('Eastern Conference Finals', '2023-2024', 'NTT10', '2022-10-24', '2026-11-23', 'Truyen thong'),
 ('NBA Finals', '2023-2024', 'NTT10', '2021-03-19', '2024-10-21', 'San pham'),
 
-('West Conf Finals', '2023-2024', 'NTT11', '2021-01-03', '2024-05-12', 'Tien'),
-('East Conf Finals', '2023-2024', 'NTT11', '2023-03-17', '2025-03-29', 'Tien'),
+('Western Conference Finals', '2023-2024', 'NTT11', '2021-01-03', '2024-05-12', 'Tien'),
+('Eastern Conference Finals', '2023-2024', 'NTT11', '2023-03-17', '2025-03-29', 'Tien'),
 
 ('NBA Finals', '2023-2024', 'NTT12', '2023-12-01', '2025-06-16', 'Truyen thong'),
-('West Conf Finals', '2023-2024', 'NTT12', '2024-01-06', '2026-02-22', 'Truyen thong'),
+('Western Conference Finals', '2023-2024', 'NTT12', '2024-01-06', '2026-02-22', 'Truyen thong'),
 
-('East Conf Finals', '2023-2024', 'NTT13', '2022-02-11', '2024-05-19', 'Truyen thong'),
+('Eastern Conference Finals', '2023-2024', 'NTT13', '2022-02-11', '2024-05-19', 'Truyen thong'),
 ('NBA Finals', '2023-2024', 'NTT13', '2023-06-01', '2024-06-01', 'Truyen thong'),
 
-('West Conf Finals', '2023-2024', 'NTT14', '2023-10-01', '2024-11-01', 'Tien'),
-('East Conf Finals', '2023-2024', 'NTT14', '2022-08-01', '2024-08-20', 'Tien'),
+('Western Conference Finals', '2023-2024', 'NTT14', '2023-10-01', '2024-11-01', 'Tien'),
+('Eastern Conference Finals', '2023-2024', 'NTT14', '2022-08-01', '2024-08-20', 'Tien'),
 
 ('NBA Finals', '2023-2024', 'NTT15', '2022-03-05', '2024-06-13', 'Tien'),
-('West Conf Finals', '2023-2024', 'NTT15', '2023-11-02', '2024-11-21', 'Tien'),
+('Western Conference Finals', '2023-2024', 'NTT15', '2023-11-02', '2024-11-21', 'Tien'),
 
-('East Conf Finals', '2023-2024', 'NTT16', '2023-02-01', '2024-05-20', 'Tien'),
+('Eastern Conference Finals', '2023-2024', 'NTT16', '2023-02-01', '2024-05-20', 'Tien'),
 ('NBA Finals', '2023-2024', 'NTT16', '2022-10-03', '2024-07-01', 'Tien'),
 
-('West Conf Finals', '2023-2024', 'NTT17', '2022-10-22', '2024-12-24', 'San pham'),
-('East Conf Finals', '2023-2024', 'NTT17', '2022-03-05', '2024-09-21', 'San pham'),
+('Western Conference Finals', '2023-2024', 'NTT17', '2022-10-22', '2024-12-24', 'San pham'),
+('Eastern Conference Finals', '2023-2024', 'NTT17', '2022-03-05', '2024-09-21', 'San pham'),
 
 ('NBA Finals', '2023-2024', 'NTT18', '2023-11-07', '2024-11-07', 'San pham'),
-('West Conf Finals', '2023-2024', 'NTT18', '2023-01-23', '2024-02-17', 'San pham'),
+('Western Conference Finals', '2023-2024', 'NTT18', '2023-01-23', '2024-02-17', 'San pham'),
 
-('East Conf Finals', '2023-2024', 'NTT19', '2022-12-09', '2026-05-03', 'San pham'),
+('Eastern Conference Finals', '2023-2024', 'NTT19', '2022-12-09', '2026-05-03', 'San pham'),
 ('NBA Finals', '2023-2024', 'NTT19', '2023-03-04', '2025-02-16', 'San pham'),
 
-('West Conf Finals', '2023-2024', 'NTT20', '2022-10-09', '2024-04-28', 'San pham'),
-('East Conf Finals', '2023-2024', 'NTT20', '2023-01-15', '2024-06-24', 'Hinh anh');
+('Western Conference Finals', '2023-2024', 'NTT20', '2022-10-09', '2024-04-28', 'San pham'),
+('Eastern Conference Finals', '2023-2024', 'NTT20', '2023-01-15', '2024-06-24', 'Hinh anh');
