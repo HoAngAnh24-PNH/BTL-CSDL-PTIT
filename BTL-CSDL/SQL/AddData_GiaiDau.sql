00700d87ca075ab670a6838330984dca1c27cba7
HoAngAnh24-PNH <phamnhuhoang24102004@gmail.com>
2024-11-18 00:23:22 +0700

    AddData_GiaiDau.sql

--- BTL-CSDL/SQL/AddData_GiaiDau.sql
+++ BTL-CSDL/SQL/AddData_GiaiDau.sql
@@ -3,8 +3,8 @@
 -- Thêm các giải đấu vào bảng GiaiDau
 INSERT INTO GiaiDau (TenGiai, MuaGiai, TheThucThiDau, BanToChuc) VALUES
 ('NBA Finals', '2023-2024', 'Single Elimination', 'NBA'),
-('West Conf Finals', '2023-2024', 'Single Elimination', 'NBA'),
-('East Conf Finals', '2023-2024', 'Single Elimination', 'NBA');
+('Western Conference Finals', '2023-2024', 'Single Elimination', 'NBA'),
+('Eastern Conference Finals', '2023-2024', 'Single Elimination', 'NBA');
 
 SELECT *
 FROM GiaiDau
